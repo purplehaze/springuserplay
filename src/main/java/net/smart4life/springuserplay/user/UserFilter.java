d3e83df681a96e4beb149ca605ea56d2075cca44
purplehaze <roman.ilin@gmail.com>
2015-03-08 20:23:29 +0100

    working on DataTable's first and sortBy restore

--- src/main/java/net/smart4life/springuserplay/user/UserFilter.java
+++ src/main/java/net/smart4life/springuserplay/user/UserFilter.java
@@ -6,11 +6,48 @@ package net.smart4life.springuserplay.user;
 public class UserFilter {
     private String name;
 
+    private int first;
+    private String sortOrder = "ascending"; // "ascending" | "descending"
+    private Object sortBy;
+    private String sortField;
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public void setFirst(int first) {
+        this.first = first;
+    }
+
+    public String getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(String sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public Object getSortBy() {
+        return sortBy;
+    }
+
+    public void setSortBy(Object sortBy) {
+        this.sortBy = sortBy;
+    }
+
+    public String getSortField() {
+        return sortField;
+    }
+
+    public void setSortField(String sortField) {
+        this.sortField = sortField;
     }
 }
